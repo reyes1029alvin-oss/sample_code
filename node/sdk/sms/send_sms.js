23100233b4f31f00fc7f05c7e49d144594efc8f8
Ben Spenard <bspenard@telesign.com>
2024-07-09 10:53:08 -0600

    PD-87376: change node sms sample to use phoneNumner, message, messageType arguments in .message call.

--- node/sdk/sms/send_sms.js
+++ node/sdk/sms/send_sms.js
@@ -2,22 +2,23 @@ const TeleSignSDK = require("telesignsdk");
 
 // Replace the defaults below with your Telesign authentication credentials or pull them from environment variables.
 const customerId =
-  process.env.CUSTOMER_ID || "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
+  process.env.CUSTOMER_ID || "408904C1-9BC4-478B-A0B3-7A3016424F8B";
 const apiKey =
   process.env.API_KEY ||
-  "ABC12345yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
+  "szRYD7CkqAYpQVjQ+Hk32XFb7zfciFXX85/cMNvJuxUcI+yWf3X9F1/KvGEBxabogHFXeYviyjat8j0h3rmfKA==";
+
 const parameters = {};
 
 // Set the default below to your test phone number or pull it from an environment variable.
 // In your production code, update the phone number dynamically for each transaction.
-parameters.phoneNumber = process.env.PHONE_NUMBER || "11234567890";
+const phoneNumber = "9714130778";
 // If you have a valid sender ID approved by Telesign, uncomment the line below and replace the placeholder value.
 // parameters.senderId = "11234567891"
 
 // Set the message text and type.
-parameters.message =
+const message =
   "Your package has shipped! Follow your delivery at https://vero-finto.com/orders/3456";
-parameters.messageType = "ARN";
+const messageType = "ARN";
 
 // Instantiate a messaging client object.
 const client = new TeleSignSDK(customerId, apiKey);
@@ -34,4 +35,4 @@ function smsCallback(error, responseBody) {
 }
 
 // Make the request and capture the response.
-client.sms.message(smsCallback, parameters);
+client.sms.message(smsCallback, phoneNumber, message, messageType, parameters);
