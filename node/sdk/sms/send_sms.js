4e3dda71f98b3a069402e97fad949b0f4e17c77e
Ben Spenard <bspenard@telesign.com>
2024-07-09 13:13:04 -0600

    PD-87376: change node sms sampe creds

--- node/sdk/sms/send_sms.js
+++ node/sdk/sms/send_sms.js
@@ -2,16 +2,15 @@ const TeleSignSDK = require("telesignsdk");
 
 // Replace the defaults below with your Telesign authentication credentials or pull them from environment variables.
 const customerId =
-  process.env.CUSTOMER_ID || "408904C1-9BC4-478B-A0B3-7A3016424F8B";
+  process.env.CUSTOMER_ID || "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
 const apiKey =
   process.env.API_KEY ||
-  "szRYD7CkqAYpQVjQ+Hk32XFb7zfciFXX85/cMNvJuxUcI+yWf3X9F1/KvGEBxabogHFXeYviyjat8j0h3rmfKA==";
-
+  "ABC12345yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
 const parameters = {};
 
 // Set the default below to your test phone number or pull it from an environment variable.
 // In your production code, update the phone number dynamically for each transaction.
-const phoneNumber = "9714130778";
+const phoneNumber = "11234567890";
 // If you have a valid sender ID approved by Telesign, uncomment the line below and replace the placeholder value.
 // parameters.senderId = "11234567891"
 
