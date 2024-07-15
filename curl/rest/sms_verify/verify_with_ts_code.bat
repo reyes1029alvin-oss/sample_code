d8451d77c23f973370efda14af400fd257537f34
Ben Spenard <bspenard@telesign.com>
2024-07-15 16:06:59 -0600

    PD-87376: change curl param

--- curl/rest/sms_verify/verify_with_ts_code.bat
+++ curl/rest/sms_verify/verify_with_ts_code.bat
@@ -3,5 +3,5 @@ curl -u "$CUSTOMER_ID":"$API_KEY" \
      --header 'accept: application/x-www-form-urlencoded' \
      --header 'content-type: application/x-www-form-urlencoded' \
      --data phone_number="$PHONE_NUMBER" \
-     # --data phone_number="$SENDER_ID" \
+     # --data sender_id="$SENDER_ID" \
      # If you have a valid sender ID approved by Telesign, uncomment the line above.
